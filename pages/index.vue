944af2698de5c7254b2e65e8853750f186d64d6a
LauraVZuluaga <49324952+LauraVZuluaga@users.noreply.github.com>
2021-12-27 13:29:38 -0500

    Initial structure for movie app

---
+++ pages/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="home">
+    <!--- Hero section---->
+    <Hero />
+
+    <!--- Movies section---->
+    <div class="container movies">
+      <div id="movie-grid">
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'IndexPage',
+  data() {
+    return {
+      movies: [],
+    }
+  },
+  async fetch() {
+    await this.getMovies()
+  },
+  methods: {
+    async getMovies() {
+      const data = axios.get(
+        `https://api.themoviedb.org/3/movie/now_playing?api_key=8887f834cb749f1169b42bc18530f525&language=en-US&page=1`
+      )
+      const result = await data
+      result.data.results.forEach((movie) => {
+        this.movies.push(movie)
+      })
+      console.log(this.movies)
+    },
+  },
+}
+</script>
